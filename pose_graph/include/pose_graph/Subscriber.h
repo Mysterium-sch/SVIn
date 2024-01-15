5357b09f7b87bcfc089ff91b65a9c5814c905afe
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-01-15 14:39:20 -0500

    division

--- pose_graph/include/pose_graph/Subscriber.h
+++ pose_graph/include/pose_graph/Subscriber.h
@@ -5,7 +5,7 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <nav_msgs/Odometry.h>
-#include <okvis_ros/SvinHealth.h>
+#include <pose_graph/SvinHealth.h>
 #include <sensor_msgs/PointCloud.h>
 
 #include <memory>
